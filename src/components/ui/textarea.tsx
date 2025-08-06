ee2e2459f461d5e55701e471e445133a546b421e
Igor Corrêa <igoracorreaa@gmail.com>
2025-08-06 20:44:22 -0300

    fix: corrige erro de linting final em textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+// AQUI ESTÁ A CORREÇÃO APLICADA
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
