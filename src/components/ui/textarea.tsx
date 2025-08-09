f8e257a7925715dabb38013aa80b038145f418b8
Igor Corrêa <igoracorreaa@gmail.com>
2025-08-09 19:32:07 -0300

    refactor: substitui interfaces vazias por type aliases para corrigir lint

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,9 +2,8 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-// AQUI ESTÁ A CORREÇÃO APLICADA
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export type TextareaProps =
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
